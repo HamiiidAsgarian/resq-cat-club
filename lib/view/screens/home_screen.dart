3e9d785c362c82818f4cd41916a6956abad19542
hamiiid <Hamiiid.Asgarian@gmail.com>
2023-02-23 19:24:20 +0330

    add fundamentals

---
+++ lib/view/screens/home_screen.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:resq_cat_club/view/widgets/private_gallary.dart';
+
+import '../widgets/public_gallary.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  List<int> notCuteEnoughList = [];
+  List<File> images = [];
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> tabs = [const PublicGallary(), const PrivateGallary()];
+
+    return DefaultTabController(
+      length: tabs.length,
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(children: [
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Text(
+                  "ResQ Cat Club",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              width: double.infinity,
+              color: Colors.blue,
+              child: Center(
+                child: TabBar(
+                    physics: const BouncingScrollPhysics(),
+                    isScrollable: true,
+                    indicatorPadding: const EdgeInsets.all(5),
+                    indicator: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.black,
+                    ),
+                    tabs: ["Online", "Private"]
+                        .map((e) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: Tab(text: e),
+                            ))
+                        .toList()),
+              ),
+            ),
+            const Expanded(
+                child:
+                    TabBarView(children: [PublicGallary(), PrivateGallary()]))
+          ]),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/widgets/dialog.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InfoDialouge extends StatefulWidget {
+  const InfoDialouge(
+      {Key? key, required this.onDelete, required this.blocedItems})
+      : super(key: key);
+  final Function onDelete;
+  final List<int> blocedItems;
+
+  @override
+  State<InfoDialouge> createState() => _InfoDialougeState();
+}
+
+class _InfoDialougeState extends State<InfoDialouge> {
+  late List<int> blocedItemsLocal;
+  @override
+  void initState() {
+    blocedItemsLocal = widget.blocedItems;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      titlePadding: const EdgeInsets.only(left: 25),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Customer Info",
+            style: TextStyle(color: Colors.red),
+          ),
+          CupertinoButton(
+            child: const Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+      children: [
+        SizedBox(
+          height: 100,
+          width: 100,
+          child: ListView.builder(
+              itemCount: blocedItemsLocal.length,
+              itemBuilder: (context, index) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(blocedItemsLocal[index].toString()),
+                        IconButton(
+                            onPressed: () {
+                              setState(() {
+                                blocedItemsLocal.removeWhere((element) =>
+                                    element == blocedItemsLocal[index]);
+                              });
+                              widget.onDelete(index);
+                            },
+                            icon: const Icon(Icons.delete))
+                      ])),
+        )
+      ],
+    );
+  }
+}
